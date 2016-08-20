5a816f9422ff2f4e172c3e2742b4558100d60c5c
Wesley A.V <wesley.contato@hotmail.com>
2016-08-20 14:12:29 -0300

    Template

---
+++ INTRODU-O-A-ALGOR-TMOS-E-L-GICA-DE-PROGRAMA-O/src/Lista1/Template.java
@@ -0,0 +1,13 @@
+package Lista1;
+
+/**
+ *
+ * @author Wesley A.V
+ */
+public class Template {
+    //declaração de variáveis
+
+    //entrada de dados
+    //processamento de dados
+    //saída de dados
+}
