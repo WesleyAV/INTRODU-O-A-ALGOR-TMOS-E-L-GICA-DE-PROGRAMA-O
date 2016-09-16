8017e046c791d2145fd7e531e854a1fab46f67c3
Wesley A.V <wesley.contato@hotmail.com>
2016-09-16 20:34:55 -0300

    Exer7

--- INTRODU-O-A-ALGOR-TMOS-E-L-GICA-DE-PROGRAMA-O/src/Lista3/Exer7.java
+++ INTRODU-O-A-ALGOR-TMOS-E-L-GICA-DE-PROGRAMA-O/src/Lista3/Exer7.java
@@ -1,7 +1,9 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Criar um programa para identificar o valor a ser pago por um plano de saúde dada a
+ * idade do conveniado considerando que todos pagam R$ 100 mais um adicional conforme a seguinte
+ * tabela: 1) crianças com menos de 10 anos pagam R$80; 2) conveniados com idade entre 10 e 30
+ * anos pagam R$50; 3) conveniados com idade entre 40 e 60 anos pagam R$ 95; e 4) conveniados
+ * com mais de 60 anos pagam R$130.
  */
 package Lista3;
 
@@ -19,39 +21,46 @@ public class Exer7 {
         double valor = 0, total;
 
         System.out.println("Informe a idade do conveniado: ");
+        System.out.println("1 - para 0 a 10: ");
+        System.out.println("2 - para 11 a 30: ");
+        System.out.println("3 - para 40 a 60: ");
+        System.out.println("4 - acima de 60: ");
+
+        System.out.println("Digite aqui: ");
         idade = scan.nextInt();
 
-        if (idade < 10) {
-            faixa = 1;
-        }
-        if (idade >= 10 && idade <= 30) {
-            faixa = 2;
-        }
-        if (idade >= 40 && idade <= 61) {
-            faixa = 3;
-        }
-        if (idade >= 60) {
-            faixa = 4;
-        }
-
-        switch (faixa) {
+        switch (idade) {
             case 1:
                 valor = 80;
+                total = valor + 100;
+                System.out.println("Valor do Plano de Saúde: R$ 100,00");
+                System.out.println("Valor Adicional por idade de R$: " + valor);
+                System.out.println("Valor Total: " + total);
                 break;
             case 2:
                 valor = 50;
+                total = valor + 100;
+                System.out.println("Valor do Plano de Saúde: R$ 100,00");
+                System.out.println("Valor Adicional por idade de R$: " + valor);
+                System.out.println("Valor Total: " + total);
                 break;
             case 3:
                 valor = 95;
+                total = valor + 100;
+                System.out.println("Valor do Plano de Saúde: R$ 100,00");
+                System.out.println("Valor Adicional por idade de R$: " + valor);
+                System.out.println("Valor Total: " + total);
                 break;
             case 4:
                 valor = 130;
+                total = valor + 100;
+                System.out.println("Valor do Plano de Saúde: R$ 100,00");
+                System.out.println("Valor Adicional por idade de R$: " + valor);
+                System.out.println("Valor Total: " + total);
+                break;
+            default:
+                System.out.println("Opção inválida! Digite novamente.");
+                break;
         }
-
-        total = valor + 100;
-        System.out.println("Valor do Plano de Saúde: R$ 100,00");
-        System.out.println("Valor Adicional por idade de : " + idade + " anos: R$ " + valor);
-        //System.out.println("%.2f", total);
-        System.out.println("Valor Total: " + total);
     }
 }
