dcb204a20d862255c0afc0d5afb3ad2e7d9b319c
Wesley A.V <wesley.contato@hotmail.com>
2016-09-16 15:34:13 -0300

    Exer8

---
+++ INTRODU-O-A-ALGOR-TMOS-E-L-GICA-DE-PROGRAMA-O/src/Lista3/Exer8.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lista3;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author admin.wesley
+ */
+public class Exer8 {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        char opcao;
+        double area = 0, base = 0, bmaior = 0, bmenor = 0, altura = 0, lado = 0;
+
+        System.out.println("Qual área você quer calcular? (q - Quadrado; r - Retângulo, t - Triangulo, p - Trapézio");
+        opcao = scan.next(".").charAt(0);
+
+        if (opcao == 'p') {
+            System.out.println("Informe a medida da base maior: ");
+            bmaior = scan.nextInt();
+
+            System.out.println("Informe a medida da base menor (em cm): ");
+            bmenor = scan.nextDouble();
+
+            System.out.println("Informe a medida do lado/altura (em cm): ");
+            altura = scan.nextDouble();
+        } else {
+            System.out.println("Informe a medida da base (em cm): ");
+            base = scan.nextDouble();
+
+            System.out.println("Informe a medida do lado/altura (em cm): ");
+            lado = scan.nextDouble();
+        }
+
+        switch (opcao) {
+            case 'q':
+                area = base * lado;
+                System.out.println("A figura escolhida foi Quadrado, e a área é de: " + area + " cm²");
+                break;
+            case 'r':
+                area = base * lado;
+                System.out.println("A figura escolhida foi Retangulo, e a área é de: " + area + " cm²");
+                break;
+            case 't':
+                area = (base * lado) / 2;
+                System.out.println("A figura escolhida foi Triangulo, e a área é de: " + area + " cm²");
+                break;
+            case 'p':
+                area = ((bmaior + bmenor) * altura) / 2;
+                System.out.println("A figura escolhida foi Trapézio, e a área é de: " + area + " cm²");
+                break;
+            default:
+                System.out.println("Figura geométrica não foi informada!");
+
+        }
+    }
+}
